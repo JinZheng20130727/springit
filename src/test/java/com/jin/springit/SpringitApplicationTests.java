6b041800467d87d130b77fee27e1dbaa0bd6d6ba
Jin Zheng <JinZheng20130727@gmail.com>
2020-04-04 10:13:13 +0800

    first commit!

---
+++ src/test/java/com/jin/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jin.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
