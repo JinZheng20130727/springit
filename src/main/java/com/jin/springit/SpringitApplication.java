04522d26992a51f0cc94f6dbf6bfcdb650a86488
Jin Zheng <JinZheng20130727@gmail.com>
2020-04-04 15:52:10 +0800

    updated

--- src/main/java/com/jin/springit/SpringitApplication.java
+++ src/main/java/com/jin/springit/SpringitApplication.java
@@ -1,14 +1,40 @@
 package com.jin.springit;
 
+import com.jin.springit.config.SpringitProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
+
+    @Autowired
+    ApplicationContext applicationContext;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
-		System.out.println("Welcome to sprintit!");
 	}
 
+	@Bean
+    CommandLineRunner runner(){
+	    return args -> {
+            log.error("error");
+            log.warn("warn");
+            log.info("info");
+            log.debug("debug");
+            log.trace("trace");
+        };
+    }
 }
